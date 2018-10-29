67f8ffc365a7dd45dd38b7984655eb531c311ef1
dragosbus <dragosbusuioc94@gmail.com>
2018-10-29 18:38:05 +0100

    refactoring using hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React, { Component } from 'react';
-import { fetchAndReload$ } from './utils';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cryptos: []
-    };
-  }
+const fetchData = async () => {
+  const data = await fetch('https://api.iextrading.com/1.0/stock/market/crypto').then(res => res.json());
 
-  componentDidMount() {
-    fetchAndReload$.subscribe(data=>{
-      this.setState(prevState=>({
-        cryptos: prevState.cryptos.concat(data)
-      }), ()=>{
-        setTimeout(()=>{
-          this.setState({cryptos:[]})
-        },4500);
-      })
-    });
-  }
+  return data.map(({ symbol, askPrice }) => ({ symbol, askPrice })).filter(coin => coin.askPrice > 50);
+};
 
-  render() {
-    return (
-      <div>
-        <ul className="stock-list">
-          {this.state.cryptos.map(coin => {
-            return (
-              <li key={coin.symbol}>
-                <h3>${coin.symbol}</h3>
-                <p>${coin.askPrice}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+const useFetchCoins = (timer) => {
+  const [cryptos, updateCryptos] = useState([]);
+
+  fetchData().then(res => {
+    if(!cryptos.length) {
+      //when the page load for the first time, we should update the state imediatelly
+      updateCryptos(res);
+    } else {
+      setTimeout(()=>{
+        //otherwise update the state with new data after x time
+        updateCryptos(res);
+      },timer);
+    }
+  });
+
+  return cryptos;
+};
+
+export default function App() {
+  const cryptos = useFetchCoins(10000);
+
+  return (
+    <div>
+      <ul className="stock-list">
+        {cryptos.map(coin => {
+          return (
+            <li key={coin.symbol}>
+              <h3>${coin.symbol}</h3>
+              <p>${coin.askPrice}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
-export default App;
